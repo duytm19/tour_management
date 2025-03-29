9a2c73ce0854b805d1507a82a6109fe8fd1ab595
duytm19 <tranminhduy1912@gmail.com>
2025-03-29 15:24:24 +0700

    tour detail

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,20 +1,20 @@
-import { Request,Response } from "express"
-import Tour from "../../models/tour.model"
-import { sequelize } from "../../config/database"
-import { QueryTypes } from "sequelize"
+import { Request, Response } from "express";
+import Tour from "../../models/tour.model";
+import { sequelize } from "../../config/database";
+import { QueryTypes } from "sequelize";
 
 // [GET] /tours/:slugCategory
-export const index = async(req:Request,res:Response)=>{
-
-    const slugCategory = req.params.slugCategory
-    // const tours = await Tour.findAll({
-    //     where:{
-    //         deleted:false,
-    //         status:"active"
-    //     },
-    //     raw:true
-    // })
-    const tours =await sequelize.query(`
+export const index = async (req: Request, res: Response) => {
+  const slugCategory = req.params.slugCategory;
+  // const tours = await Tour.findAll({
+  //     where:{
+  //         deleted:false,
+  //         status:"active"
+  //     },
+  //     raw:true
+  // })
+  const tours = await sequelize.query(
+    `
         SELECT tours.*, price * (1-discount/100) AS price_special
         FROM tours
         JOIN tours_categories ON tours.id = tours_categories.tour_id
@@ -25,19 +25,46 @@ export const index = async(req:Request,res:Response)=>{
             and categories.status = 'active'
             AND tours.deleted = false
             AND tours.status = 'active'
-        `,{
-            type:QueryTypes.SELECT
-        })
+        `,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
 
-    tours.forEach(item =>{
-        if(item['images']){
-            const images = JSON.parse(item["images"])
-            item["image"] = images[0]
-        }
-        item["price_special"] = parseFloat(item["price_special"])
-    })
-    res.render('client/pages/tours/index',{
-        pageTitle:"Tours",
-        tours:tours
-    })
-}
+  tours.forEach((item) => {
+    if (item["images"]) {
+      const images = JSON.parse(item["images"]);
+      item["image"] = images[0];
+    }
+    item["price_special"] = parseFloat(item["price_special"]);
+  });
+  res.render("client/pages/tours/index", {
+    pageTitle: "Tours",
+    tours: tours,
+  });
+};
+
+// [GET] /tours/detail/:slugTour
+
+export const detail = async (req: Request, res: Response) => {
+  const slugTour = req.params.slugTour;
+
+  const tourDetail = await Tour.findOne({
+    where: {
+      slug: slugTour,
+      deleted: false,
+      status: "active",
+    },
+    raw:true
+  });
+  if (tourDetail["images"]) {
+    tourDetail["images"] = JSON.parse(tourDetail["images"]);
+  }
+  tourDetail["price_special"] = tourDetail["price"]*(1-tourDetail["discount"]/100)
+
+ //console.log(tourDetail)
+  res.render("client/pages/tours/detail",{
+    pageTitle:"Tour Detail",
+    tourDetail:tourDetail
+  })
+};
