f1fa29734dd4c2fa11e14950574b07f8f75c2ae3
duytm19 <tranminhduy1912@gmail.com>
2025-03-29 18:09:31 +0700

    add tours to cart

--- public/js/script.js
+++ public/js/script.js
@@ -19,3 +19,41 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 // End swiper
+
+// Carts
+// Create cart in local storage if the system don't contain local storage
+const cart = localStorage.getItem("cart")
+if(!cart){
+    localStorage.setItem("cart",JSON.stringify([]))
+}
+// Add tour in to cart
+const formAddToCart = document.querySelector("[form-add-to-cart]")
+if(formAddToCart){
+    formAddToCart.addEventListener("submit",(e)=>{
+        e.preventDefault()
+
+        const quantity = parseInt(e.target.elements.quantity.value)
+        const tourId = parseInt(formAddToCart.getAttribute("tour-id"))
+
+        if(quantity>0 && tourId){
+
+            const cart = JSON.parse(localStorage.getItem("cart"))
+
+            const indexExistTour = cart.findIndex(item => item.tourId == tourId)
+
+            if(indexExistTour == -1){
+                cart.push({
+                    tourId:tourId,
+                    quantity:quantity
+                })
+            }
+            else{
+                cart[indexExistTour].quantity = cart[indexExistTour].quantity +quantity
+            }
+            localStorage.setItem("cart",JSON.stringify(cart))
+        }
+    })
+
+}
+
+// End Carts
